5629dd4e05d422ebc95db8fc212269f8df59df94
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-09-16 15:43:43 +0000

    4.1.0

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,104 +1,100 @@
-import React, { useCallback, useEffect, useState, } from 'react'
-import 'quill/dist/quill.snow.css'
-import Quill from 'quill';
-import { io } from "socket.io-client"
-import { useParams } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from "react";
+import "quill/dist/quill.snow.css";
+import Quill from "quill";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
-const Save_Interval= 2000;
+const Save_Interval = 2000;
 const Toolbar_options = [
-    [{header: [1, 2, 3, 4, 5, 6, false]}],
-    [{font: [ ] }],
-    [{list: "ordered"}, {list: "bullet"}],
-    ["bold", "italic", "underline"],
-    [{color: []}, {background: [] }],
-    [{ script: "sub"}, {script:"super"}],
-    ["clean"],
-
-]
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  ["clean"],
+];
 
 export default function TextEditor() {
-    const {id: documentId} = useParams()
-    const [socket, setSocket]= useState()
-    const [quill, setQuill]= useState()
-    // console.log (documentId)
-    // <--connect to server-->
-    useEffect(() => {
-    const s =  io("http://localhost:8080")     
-    setSocket(s)         
-    
+  const { id: documentId } = useParams();
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  // console.log (documentId)
+  // <--connect to server-->
+  useEffect(() => {
+    const s = io(process.env.BACKEND_URL);
+    setSocket(s);
+
     // <--disconnect from server-->
     return () => {
-        s.disconnect()
-    }
-    },[])
+      s.disconnect();
+    };
+  }, []);
 
-    useEffect (() => {
-        if (socket==null || quill==null) return
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-        socket.once('load-document', document => {
-            quill.setContents(document)
-            quill.enable()
-        })
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
 
-        socket.emit('get-document', documentId)
-    },[socket,quill, documentId])
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
 
-    useEffect(() => {
-        if (socket == null || quill == null ) return
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-        const interval = setInterval(() =>{
-            socket.emit('save-document', quill.getContents())
-        }, Save_Interval)
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, Save_Interval);
 
-        return () => {
-            clearInterval(interval)
-        }
-    }, [socket, quill])
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
 
-    //live changing of text at different computers
-    useEffect(() => {
-        if (socket == null || quill == null ) return
-        
-        const handler= delta => {
-            quill.updateContents(delta)
-        }
-        socket.on('receive-changes', handler )
-        return () => {
-            socket.off('receive-changes', handler)
-        }
-    },[socket, quill])
+  //live changing of text at different computers
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
 
-// detect the changes made by the user or not
-    useEffect(() => {
-        if (socket == null || quill == null ) return
+  // detect the changes made by the user or not
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-        const handler= (delta, oldDelta, source) => {
-            if (source!== 'user') return
-            socket.emit('send-changes', delta)
-        }
-        quill.on('text-change', handler )
-        return () => {
-            quill.off('text-change', handler)
-        }
-    },[socket, quill])
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
 
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: Toolbar_options },
+    });
+    q.disable(false);
+    q.setText("Loading...");
+    setQuill(q);
+  }, []);
 
-    const wrapperRef = useCallback((wrapper) => {
-        if ( wrapper == null ) return
-        wrapper.innerHTML=''
-        const editor= document.createElement('div')
-        wrapper.append(editor)
-        const q = new Quill(editor, { theme: "snow", modules: {toolbar: Toolbar_options } })
-        q.disable(false)
-        q.setText('Loading...')
-        setQuill(q)
-    },[])
-
-    // rendering of page
-    return (
-        <div className="container" ref={wrapperRef}>
-        
-        </div>
-    )
+  // rendering of page
+  return <div className="container" ref={wrapperRef}></div>;
 }
