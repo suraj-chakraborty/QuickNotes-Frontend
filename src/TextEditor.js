ef4cb7ee77134db834582a59567a05a2c6e1dd59
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-09-18 22:06:35 +0000

    error fix 3.2

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -23,14 +23,14 @@ export default function TextEditor() {
   // <--connect to server-->
   useEffect(() => {
     const s = io("https://cute-pear-newt-tux.cyclic.app/", {
-      transports: ["polling", "websocket"],
+      transports: ["websocket", "polling"],
     });
     setSocket(s);
 
     // <--disconnect from server-->
-    return () => {
-      s.disconnect();
-    };
+    // return () => {
+    //   s.disconnect();
+    // };
   }, []);
 
   useEffect(() => {
